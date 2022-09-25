60ef167526f3555a942f3050b55f72713e817489
snakepatch53 <snakepatch53@gmail.com>
2022-09-25 16:51:15 -0500

    cambio de calidad de imagenes

--- control/script.public/imageviewer.js
+++ control/script.public/imageviewer.js
@@ -9,7 +9,10 @@ function showPhotoInImageviewer(index) {
     const photo_name = database[index].photo_name;
     const photo_beff = index - 1 <= 0 ? database.length - 1 : index - 1;
     const photo_next = index + 1 >= database.length ? 0 : index + 1;
-    $imageviewermodal.querySelector("img").src = `${$proyect.url}view/img/photo/${photo_name}`;
+    // $imageviewermodal.querySelector("img").src = `${$proyect.url}view/img/photo/${photo_name}`;
+    $imageviewermodal.querySelector(
+        "img"
+    ).src = `${$proyect.url}model/script/photo/getphoto.php?photo_name=${photo_name}&photo_quality=30&photo_width=800`;
     $buttonBeff.setAttribute("image-index", photo_beff);
     $buttonNext.setAttribute("image-index", photo_next);
     bootstrap_modalViewPhoto.show();
